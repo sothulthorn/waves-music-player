a25865b5534e9ad538561300d54ed145c1fcd5d6
sothulthorn <thornsothul@gmail.com>
2024-08-20 19:56:37 +0900

    Library UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
 
 import './styles/app.scss';
 
@@ -19,6 +20,7 @@ function App() {
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
       />
+      <Library songs={songs} />
     </div>
   );
 }
